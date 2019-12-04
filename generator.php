f9319a7d0763c485e3cd027989ebe2b38457df12
gettmure <myshalov.re@students.dvfu.ru>
2019-12-04 15:29:48 +1000

    save commit

--- generator.php
+++ generator.php
@@ -1,84 +1,105 @@
 <?php
+require 'vendor/autoload.php';
+
+use Ramsey\Uuid\Uuid;
+
+const CATEGORIES_COUNT = 5000;
+const USERS_COUNT = 500000;
+const MESSAGES_COUNT = 1000000;
+const MAX_ROWS = 3000;
+
+$db = new PDO("pgsql:host=localhost; dbname=generator", "postgres", "1234");
+$categoriesUUID = [];
+$usersUUID = [];
+$messagesUUID = [];
+
+function generateUuid()
+{
+    return Uuid::uuid4()->toString();
+}
+
+function generateUuidArray(&$array, $count)
+{
+    for ($i = 0; $i < $count; $i++) {
+        $array['id' . $i] = generateUuid();
+    }
+}
+
+function generateCategoriesTable($categories, $count, $db)
+{
+    generateUuidArray($categories, $count);
+    $chunks = array_chunk($categories, MAX_ROWS, true);
+
+    $startIndex = 0;
+    foreach ($chunks as $chunk) {
+        $sql = "INSERT INTO categories(id, name) VALUES";
+        $insertQuery = [];
+
+        for ($i = 0; $i < MAX_ROWS; $i++) {
+            $index = $startIndex + $i;
+            $insertQuery[] = sprintf("(:id%d, 'Category #%d')", $index, $index);
+        }
+
+        $sql .= implode(',', $insertQuery) . ";";
+        $db->prepare($sql)->execute($chunk);
+        $startIndex += MAX_ROWS;
+    }
+}
+
+function generateUsersTable($users, $count, $db)
+{
+    generateUuidArray($users, $count);
+    $chunks = array_chunk($users, MAX_ROWS, true);
+
+    $startIndex = 0;
+    foreach ($chunks as $chunk) {
+        $sql = "INSERT INTO users(id, name) VALUES";
+        $insertQuery = [];
+
+        for ($i = 0; $i < MAX_ROWS; $i++) {
+            $index = $startIndex + $i;
+            $insertQuery[] = sprintf("(:id%d, 'User #%d')", $index, $index);
+        }
+
+        $sql .= implode(',', $insertQuery) . ";";
+        $db->prepare($sql)->execute($chunk);
+        $startIndex += MAX_ROWS;
+    }
+}
+
+function generateMessagesTable($messages, $categories, $users, $count, $db) {
+    generateUuidArray($messages, $count);
+    print(var_dump($categories) . PHP_EOL);
+    print(array_rand($categories) . PHP_EOL);
+    print(var_dump($categories));
+    // $chunks = array_chunk($messages, MAX_ROWS, true);
+
+    // $startIndex = 0;
+    // foreach ($chunks as $chunk) {
+    //     $sql = "INSERT INTO messages(id, text, category_id, posted_at, author_id) VALUES";
+    //     $insertQuery = [];
+
+    //     for ($i = 0; $i < MAX_ROWS; $i++) {
+    //         $messageIndex = $startIndex + $i;
+    //         $randomCategoryIndex = array_rand($categories);
+    //         print(var_dump($categories));
+    //         $randomUsersIndex = array_rand($users);
+    //         print($randomCategoryIndex . $randomUsersIndex . PHP_EOL);
+    //         $insertQuery[] = sprintf("(:id%d, 'Text #%d', '%s', '00:00:00', '%s')", $messageIndex, $messageIndex, $categories[$randomCategoryIndex], $users[$randomUsersIndex]);
+    //     }
+
+    //     $sql .= implode(',', $insertQuery) . ";";
+    //     $db->prepare($sql)->execute($chunk);
+    //     $startIndex += MAX_ROWS;
+    // }
+}
+
 $start = microtime(true);
 
-function randomUUID()
-{
+generateCategoriesTable($categoriesUUID, CATEGORIES_COUNT, $db);
+generateUsersTable($usersUUID, USERS_COUNT, $db);
 
-    return sprintf(
-        '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-        mt_rand(0, 0xffff),
-        mt_rand(0, 0xffff),
-        mt_rand(0, 0xffff),
-        mt_rand(0, 0x0fff) | 0x4000,
-        mt_rand(0, 0x3fff) | 0x8000,
-        mt_rand(0, 0xffff),
-        mt_rand(0, 0xffff),
-        mt_rand(0, 0xffff)
-    );
-}
+print(var_dump($categoriesUUID) . PHP_EOL . var_dump($usersUUID));
+// generateMessagesTable($messagesUUID, $categoriesUUID, $usersUUID, MESSAGES_COUNT, $db);
 
-function generateUUID(&$array)
-{
-    for ($i = 0; $i < sizeof($array); $i++) {
-        $array[$i] = randomUUID();
-    }
-}
-
-function generateCategoriesTable()
-{
-    global $categoriesUUID, $pdo;
-    generateUUID($categoriesUUID);
-    $sql = "INSERT INTO categories(id, name) VALUES";
-    for ($i = 0; $i < sizeof($categoriesUUID); $i++) {
-        $sql = $sql . "('$categoriesUUID[$i]', '$i'),";
-    }
-    $sql[strlen($sql) - 1] = ";";
-    $pdo->query($sql);
-}
-
-function generateUsersTable()
-{
-    global $usersUUID, $pdo;
-    generateUUID($usersUUID);
-    $i = 0;
-    while ($i < sizeof($usersUUID)) {
-        $sql = "INSERT INTO users(id, name) VALUES";
-        for ($j = 0; $j < 3125; $j++) {
-            $userIndex = $i + $j;
-            $sql = $sql . "('$usersUUID[$userIndex]', '$userIndex'),";
-        }
-        $sql[strlen($sql) - 1] = ";";
-        $pdo->query($sql);
-        $i += 3125;
-    }
-}
-
-function generateMessagesTable()
-{
-    global $messagesUUID, $categoriesUUID, $usersUUID, $pdo;
-    generateUUID($messagesUUID);
-    $i = 0;
-    while ($i < sizeof($messagesUUID)) {
-        $sql = "INSERT INTO messages(id, text, category_id, posted_at, author_id) VALUES";
-        for ($j = 0; $j < 3125; $j++) {
-            $messageIndex = $i + $j;
-            $categoryIndex = array_rand($categoriesUUID);
-            $userIndex = array_rand($usersUUID);
-            $sql = $sql . "('$messagesUUID[$messageIndex]', '$messageIndex', '$categoriesUUID[$categoryIndex]', '00:00:00', '$usersUUID[$userIndex]'),";
-        }
-        $sql[strlen($sql) - 1] = ";";
-        $pdo->query($sql);
-        $i += 3125;
-    }
-}
-
-$categoriesUUID = array_fill(0, 5000, NULL);
-$usersUUID = array_fill(0, 500000, NULL);
-$messagesUUID = array_fill(0, 1000000, NULL);
-
-$pdo = new PDO("pgsql:host=localhost; dbname=forum_generator", "postgres", "1234");
-
-generateCategoriesTable();
-generateUsersTable();
-generateMessagesTable();
 printf("Done for %.2f seconds" . PHP_EOL, microtime(true) - $start);
